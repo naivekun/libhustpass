4fcc5c6c4fee61eb6d6374d2c7dd28bd1ba2116a
Jarmo Hu <34189831+JarmoHu@users.noreply.github.com>
2020-11-05 23:57:51 +0800

    Update captcha.py

--- libhustpass/captcha.py
+++ libhustpass/captcha.py
@@ -2,9 +2,9 @@ from PIL import Image, GifImagePlugin, ImageFilter
 from pytesseract import image_to_string
 
 def Fuckit(imageContent):
-    imageObject = Image.open(imageContent)
-    imageObject.seek(1)
-    grayImage = imageObject.convert("L")
+    with Image.open(imageContent) as imageObject:
+        imageObject.seek(1)
+        grayImage = imageObject.convert("L")
     binarizedImage = grayImage.point(lambda i: i == 255 and 255)
     depointedImage = binarizedImage.filter(ImageFilter.MedianFilter(3))
     code = image_to_string(depointedImage, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
